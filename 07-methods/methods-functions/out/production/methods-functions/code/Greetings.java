a32ea025150a94cdaf5996e553e0fb07a9012ff0
Syed Asimuddin <116266965+Asimsyed42@users.noreply.github.com>
2025-09-03 10:11:01 +0530

    Add files via upload

---
+++ 07-methods/methods-functions/out/production/methods-functions/code/Greetings.java
@@ -0,0 +1,11 @@
+package code;
+
+public class Greetings {
+    public static void main(String[] args) {
+        greeting();
+    }
+    static void greeting(){
+        System.out.println("hello world");
+    }
+
+}
