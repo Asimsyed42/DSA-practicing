e670d3ecb89bc7a88daa62717dda3d06d625dcc8
Syed Asimuddin <116266965+Asimsyed42@users.noreply.github.com>
2025-09-27 23:59:37 +0530

    Add files via upload

---
+++ 08-arrays/Arrays/code/out/production/code/Assignment/MaxRow.java
@@ -0,0 +1,23 @@
+package Assignment;
+public class MaxRow {
+    static int maximumWealth(int[][] accounts) {
+        int max = 0;
+
+        for (int i=0;i<accounts.length;i++){
+            int sum=0;
+            for (int j=0;j<accounts[i].length;j++){
+                sum = sum+accounts[i][j];
+                if(sum>max){
+                    max = sum;
+                }
+            }
+        }
+
+        return max;
+    }
+    public static void main(String[] args){
+        int[][] accounts = {{1,5},{7,3},{3,5}};
+        int richest = maximumWealth(accounts);
+        System.out.print(richest);
+    }
+}
