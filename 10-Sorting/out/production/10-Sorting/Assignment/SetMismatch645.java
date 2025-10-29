8df54423f70d2f610c076b7034bc45757e530b9a
Syed Asimuddin <116266965+Asimsyed42@users.noreply.github.com>
2025-10-29 22:16:46 +0530

    Add files via upload

---
+++ 10-Sorting/out/production/10-Sorting/Assignment/SetMismatch645.java
@@ -0,0 +1,35 @@
+package Assignment;
+
+import java.util.Arrays;
+
+public class SetMismatch645 {
+    public static void main(String[] args) {
+        int[] nums = {1,2,2,4};
+        int [] ans = findErrorNums(nums);
+        System.out.println(Arrays.toString(ans));
+    }
+    static int [] findErrorNums(int[] nums){
+        int i = 0;
+        while(i < nums.length){
+            int correct = nums[i]-1;
+            if (nums[i]!=nums[correct]){
+                swap(nums, i , correct);
+            }else{
+                i++;
+            }
+        }
+        int [] ans = new int[2];
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index+1) {
+                ans[0]=nums[index];
+                ans [1] = index+1;
+            }
+        }
+        return ans ;
+    }
+    static void swap(int[] arr, int first, int second) {
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
