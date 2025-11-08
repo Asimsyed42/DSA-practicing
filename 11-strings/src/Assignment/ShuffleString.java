6df0218eeecd81ddabddbe529a7d2c89ab67a51f
Syed Asimuddin <116266965+Asimsyed42@users.noreply.github.com>
2025-11-08 17:22:24 +0530

    Add files via upload

---
+++ 11-strings/src/Assignment/ShuffleString.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+public class ShuffleString {
+    public static void main(String[] args) {
+
+    }
+    public String restoreString(String s, int[] indices) {
+        char[] result = new char[s.length()];
+
+        // Place each character at its correct index
+        for (int i = 0; i < s.length(); i++) {
+            result[indices[i]] = s.charAt(i);
+        }
+        
+        return new String(result);
+    }
+}
