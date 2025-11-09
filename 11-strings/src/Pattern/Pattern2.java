a028d32eb9e98aed147fa036bd4ba1d831ea8481
Syed Asimuddin <116266965+Asimsyed42@users.noreply.github.com>
2025-11-09 21:46:25 +0530

    Add files via upload

---
+++ 11-strings/src/Pattern/Pattern2.java
@@ -0,0 +1,21 @@
+package Pattern;
+//        *
+//        **
+//        ***
+//        ****
+//        *****
+public class Pattern2 {
+    public static void main(String[] args) {
+        pattern2(5);
+    }
+    static void pattern2(int n ){
+        for (int row = 1; row <= n; row++) {
+            //for every row, run the column
+            for (int col = 1; col <= row ; col++) {
+                System.out.print("* ");
+            }
+            // when row is printed, we need to add a newline
+            System.out.println();
+        }
+    }
+}
