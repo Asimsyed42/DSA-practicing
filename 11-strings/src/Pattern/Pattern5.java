20d91fbed3f0f8cc7e374200726e5b6d9d289942
Syed Asimuddin <116266965+Asimsyed42@users.noreply.github.com>
2025-11-10 21:12:16 +0530

    Add files via upload

---
+++ 11-strings/src/Pattern/Pattern5.java
@@ -0,0 +1,30 @@
+package Pattern;
+//        *
+//        * *
+//        * * *
+//        * * * *
+//        * * * * *
+//        * * * *
+//        * * *
+//        * *
+//        *
+public class Pattern5 {
+    public static void main(String[] args) {
+        pattern5(5);
+    }
+    static void pattern5(int n ){
+        for (int row = 0; row < 2 * n; row++) {
+            int totalColInRow;
+            if (row>n){
+                totalColInRow=2*n-row;
+            }else {
+                totalColInRow = row;
+            }
+            for (int col = 0; col <totalColInRow ; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
