b4bccb23fc621700c8a1e2724feb64f5dd45d3b4
Syed Asimuddin <116266965+Asimsyed42@users.noreply.github.com>
2026-02-13 22:21:16 +0530

    Add files via upload

---
+++ 18-Heaps/src/Main.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Heap<Integer> heap = new Heap<>();
+
+        heap.insert(34);
+        heap.insert(45);
+        heap.insert(22);
+        heap.insert(89);
+        heap.insert(76);
+
+        ArrayList list = heap.heapSort();
+        System.out.println(list);
+
+    }
+}
