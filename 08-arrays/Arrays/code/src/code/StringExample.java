c92bcdb28798f67ca63b42349fdb9e59fed7236e
Syed Asimuddin <116266965+Asimsyed42@users.noreply.github.com>
2026-04-15 22:19:17 +0530

    Add files via upload

--- 08-arrays/Arrays/code/src/code/StringExample.java
+++ 08-arrays/Arrays/code/src/code/StringExample.java
@@ -1,18 +1,13 @@
 package code;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class StringExample {
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+        String message = greet();
+        System.out.println(message);
+    }
 
-        String[] str = new String[4];  // Now this refers to java.lang.String
-        for (int i = 0; i < str.length; i++) {
-            str[i] = in.next();
-        }
-        System.out.println(Arrays.toString(str));
-        str[0]="Asim";
-        System.out.println(Arrays.toString(str));
+    static String greet(){
+        String greet = "how are you ?";
+        return greet;
     }
 }
